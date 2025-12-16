7aa6f85efc94ef9b0b549a8a9526cdd1dd43ecbc
rizzsv <rizq.syafriano@gmail.com>
2025-12-16 12:08:00 +0700

    feat: implement access logger middleware for enhanced request logging

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { accessLogger } from "./core/logger/access.logger";
+import { accessLoggerMiddleware } from "./core/logger/access.midleware";
 import { appLogger } from "./core/logger/app.logger";
 // import routes from "./routes";
 
@@ -11,4 +11,8 @@ const app = express();
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+
+app.use(accessLoggerMiddleware)
+
+app.use(morgan('dev'))
 
