b233d96e805fb276bc224f09f8290f72031a1b9e
rizzsv <rizq.syafriano@gmail.com>
2025-12-17 15:58:36 +0700

    feat(disaster): implement disaster management module with create, resolve, and retrieval functionalities

--- src/api/api.private.ts
+++ src/api/api.private.ts
@@ -3,6 +3,7 @@ import { authenticate } from '../middlewares/auth.middleware';
 import { UserController } from '../modules/user/user.controller';
 import { authorize } from '../middlewares/role.middleware';
 import { AuthController } from '../modules/user/auth.controller';
+import { DisasterController } from '../modules/disaster/disaster.controller';
 const PrivateApi = Router();
 
 PrivateApi.use(authenticate);
@@ -17,5 +18,8 @@ PrivateApi.get('/users/:id', UserController.findById);
 PrivateApi.put('/users/update/:id', UserController.update);
 PrivateApi.delete('/users/deleted/:id', UserController.delete);
 
+// Disaster Management Routes (admin and volunteer)
+PrivateApi.post('/disasters/create', authorize('ADMIN', 'VOLUNTEER'), DisasterController.create);
+PrivateApi.patch('/disasters/:id/resolve', authorize('ADMIN'), DisasterController.resolve);
 
 export default PrivateApi;
