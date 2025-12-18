4d5520b3493ffbd615b0e799abf0b1fe3a42661b
rizzsv <rizq.syafriano@gmail.com>
2025-12-18 10:48:01 +0700

    feat(push-subscription): implement PushSubscription functionality with subscribe and unsubscribe endpoints

--- src/api/api.public.ts
+++ src/api/api.public.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ReportController } from '../modules/report/report.controller';
 import { AuthController } from '../modules/user/auth.controller';
 import { DisasterController } from '../modules/disaster/disaster.controller';
+import { PushSubscriptionController } from '../modules/push-subscription/push-subscription.controller';
 
 const publicApi = Router();
 
@@ -13,5 +14,9 @@ publicApi.post('/reports', ReportController.create);
 
 // Public Disaster Routes
 publicApi.get('/disasters', DisasterController.getActive);
+
+// Public Subscription Routes
+publicApi.post('/push/subscribe', PushSubscriptionController.subscribe);
+publicApi.post('/push/unsubscribe', PushSubscriptionController.unsubscribe);
 
 export default publicApi;
